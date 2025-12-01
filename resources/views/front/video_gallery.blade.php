e110cc8e72510d16cf5852951ec642ddf96040d7
Khaiqol <Khaiqol@gmail.com>
2025-12-01 10:05:12 +0800

    video blade

--- resources/views/front/video_gallery.blade.php
+++ resources/views/front/video_gallery.blade.php
@@ -10,7 +10,7 @@
                 <nav class="breadcrumb-container">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">News Video</li>
+                        <li class="breadcrumb-item active" aria-current="page">Video</li>
                     </ol>
                 </nav>
             </div>
